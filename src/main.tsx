006861dca2d9f1c3f8b82d52a95c453567fbaa7f
Elena Peshevich <milena125212@gmail.com>
2025-10-03 20:32:24 +0300

    Change

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const store = setupStore();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/React-project-films">
           <App />
       </BrowserRouter>
     </Provider>
